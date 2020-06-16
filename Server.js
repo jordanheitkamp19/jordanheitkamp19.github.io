a31e00f0dd2915f05656d92f828f9b4290e229dd
Jordan Heitkamp <j-heitkamp@onu.edu>
2020-06-15 20:46:54 -0400

    Starts Server onload of HTML

--- Server.js
+++ Server.js
@@ -6,22 +6,24 @@ var LISTENING_PORT_NUMBER = 32000;
 
 const http = require( 'http' );
 
-//Request takes the clients post
-//responseponse sends back to the client
-const server = http.createServer( function( request, response ) {
-    response.setHeader( 'Content-type', 'application/json' );
-    response.setHeader( 'Access-Control-Allow-Origin', '*' );
-    response.writeHead( 200 ); //status code HTTP 200 /OK
+var startServer = function() {
+    //Request takes the clients post
+    //responseponse sends back to the client
+    const server = http.createServer( function( request, response ) {
+        response.setHeader( 'Content-type', 'application/json' );
+        response.setHeader( 'Access-Control-Allow-Origin', '*' );
+        response.writeHead( 200 ); //status code HTTP 200 /OK
 
-    var dataObj = {
-        id: 123,
-        name: "bob",
-        email: "bob@work.org"
-    };
+        var dataObj = {
+            id: 123,
+            name: "bob",
+            email: "bob@work.org"
+        };
 
-    response.end( JSON.stringify( dataObj ) );
-} );
+        response.end( JSON.stringify( dataObj ) );
+    } );
 
-server.listen( LISTENING_PORT_NUMBER, function() {
-    console.log( `Listening on port: ${LISTENING_PORT_NUMBER}` );
-} );
+    server.listen( LISTENING_PORT_NUMBER, function() {
+        console.log( `Listening on port: ${LISTENING_PORT_NUMBER}` );
+    } );
+}
